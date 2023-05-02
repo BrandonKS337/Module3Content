21b58fa81d9bc6fead0395a5855057e0a6abc802
Project V2 <brandonks337@gmail.com>
2023-05-01 20:54:17 -0500

    removed unnecessary folder duplicating things inside module3Content

--- 3.1JS files/JS_labs/advancedlab.js
+++ 3.1JS files/JS_labs/advancedlab.js
@@ -7,15 +7,18 @@
 // c) Modify makeCounter to take another argument incrementBy, which specifies how
 // much each call to counter() should increase the counter value by.
 
-function makeCounter() {
-  let currentCount = 0;
-  return function () {
-    currentCount++;
-    console.log(currentCount);
-    return currentCount;
-  };
-}
-function makeCounter() {
+// function makeCounter() {                   // commented out to move it into section b
+//   let currentCount = 0;
+//   return function () {
+//     currentCount++;
+//     console.log(currentCount);
+//     return currentCount;
+//   };
+// }
+
+// a) Create a second counter counter2 using the makeCounter function and test to
+// see if it remains independent to counter1
+function counter2() {
     let currentCount = 0;
     return function () {
       currentCount++;
@@ -23,5 +26,21 @@ function makeCounter() {
       return currentCount;
     };
   }
+
+  // b) Modify makeCounter so that it takes an argument startFrom specifying where
+// the counter starts from (instead of always starting from 0)
+function makeCounter(startFrom) {
+    let currentCount = 0;
+    let newStart = (currentCount + startFrom)
+    return function () {
+      currentCount++;
+      console.log(currentCount);
+      console.log(newStart)
+      return currentCount;
+      
+    };
+  }
+
+console.log(makeCounter(4))
 let counter1 = makeCounter();
 counter1(); // 1 counter1(); // 2
