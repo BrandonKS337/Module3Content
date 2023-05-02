1565bb22ee70cfbbc69c5b862e7749a0840df13c
Project V2 <brandonks337@gmail.com>
2023-05-01 21:44:59 -0500

    problem 1 answered. 9 more to gogit add .

--- 3.1JS files/JS_labs/advancedlab.js
+++ 3.1JS files/JS_labs/advancedlab.js
@@ -7,40 +7,48 @@
 // c) Modify makeCounter to take another argument incrementBy, which specifies how
 // much each call to counter() should increase the counter value by.
 
-// function makeCounter() {                   // commented out to move it into section b
-//   let currentCount = 0;
-//   return function () {
-//     currentCount++;
-//     console.log(currentCount);
-//     return currentCount;
-//   };
-// }
-
+function makeCounter() {
+  let currentCount = 0;
+  return function () {
+    currentCount++;
+    console.log(currentCount);
+    return currentCount;
+  };
+}
 // a) Create a second counter counter2 using the makeCounter function and test to
 // see if it remains independent to counter1
-function counter2() {
-    let currentCount = 0;
-    return function () {
-      currentCount++;
-      console.log(currentCount);
-      return currentCount;
-    };
-  }
-
-  // b) Modify makeCounter so that it takes an argument startFrom specifying where
+// AND b) Modify makeCounter so that it takes an argument startFrom specifying where
 // the counter starts from (instead of always starting from 0)
-function makeCounter(startFrom) {
-    let currentCount = 0;
-    let newStart = (currentCount + startFrom)
-    return function () {
-      currentCount++;
-      console.log(currentCount);
-      console.log(newStart)
-      return currentCount;
-      
-    };
+// AND c) Modify makeCounter to take another argument incrementBy, which specifies how
+// much each call to counter() should increase the counter value by.
+
+function makeCounter(startFrom = 0, incrementBy) {
+  // sets default "start" at zero unless argument entered
+  let count = startFrom;
+
+  function counter() {
+    count++;
+    return count;
+  }
+  function counter() {
+    count += incrementBy;
+    return count;
   }
 
-console.log(makeCounter(4))
-let counter1 = makeCounter();
-counter1(); // 1 counter1(); // 2
+  return counter;
+}
+
+const counter1 = makeCounter(1, 4); //counter1 = makeCounter(start #, add how many to start)
+const counter2 = makeCounter(5, 1); 
+
+
+//testing console logging counter multiple times for part a to see them in action as they are called
+// multiple Time
+                         // Note: If left empty these below log as stated.
+console.log(counter1()); // logs Output 1 
+console.log(counter1()); // logs Output 2
+console.log(counter1()); // logs Output 3
+
+console.log(counter2()); // logs Output 1    
+console.log(counter2()); // logs Output 2    
+console.log(counter2()); // logs Output 3     
